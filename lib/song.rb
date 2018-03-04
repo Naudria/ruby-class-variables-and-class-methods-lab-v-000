42a2165d7d3ba83d13858a7caec6efd619522d7d
Rima Jean <rima.jean@gmail.com>
2018-03-04 22:34:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,8 +33,11 @@ class Song
   @@artist_count
 end
 
-  def self.genre_count
-
+def self.genre_count
+  @@genres.each do |genre|
+    @@genre_count[genre] = @@genres.count(genre)
   end
+  @@genre_count
+end
 
 end
