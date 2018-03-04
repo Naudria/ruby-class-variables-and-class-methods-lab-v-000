e8ff5ecd8bf62ab368aec769ef91cd772e8b26b5
Rima Jean <rima.jean@gmail.com>
2018-03-04 22:40:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,46 +1,55 @@
 class Song
   attr_accessor :name, :artist, :genre
-  attr_reader :artist
 
   @@count = 0
   @@genres = []
   @@artists = []
-  @@genre_count = {}
-  @@artist_count = {}
 
-  def initialize(name, genre, artist)
+  def initialize(name, artist, genre) #takes 3 args
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
-    @@genres << genre
-    @@artists << artist
+    @@genres.push(@genre)
+    @@artists.push(@artist)
   end
 
   def self.count
-    @@count
-  end
-
-  def self.artists
-    @@artists.uniq
+    return @@count
   end
 
   def self.genres
     @@genres.uniq
   end
 
-  def self.artist_count
-    @@artists.each do |artist|
-      @@artist_count[artist] = @@artists.count(artist)
+  def self.artists
+    @@artists.uniq
   end
-  @@artist_count
+
+  def self.genre_count
+    genre_hash = Hash.new()
+
+    @@genres.each do |genre|
+      if genre_hash[genre]
+      genre_hash[genre] += 1
+    else
+      genre_hash[genre] = 1
+    end
+  end
+  genre_hash
 end
 
-def self.genre_count
-  @@genres.each do |genre|
-    @@genre_count[genre] = @@genres.count(genre)
+def self.artist_count
+  artist_hash = Hash.new()
+
+  @@artists.each do |artist|
+    if artist_hash[artist]
+    artist_hash[artist] += 1
+  else
+    artist_hash[artist] = 1
   end
-  @@genre_count
+end
+artist_hash
 end
 
 end
